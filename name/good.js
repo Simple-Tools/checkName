537e94b221c9de3bda2a63f362b8767b01972be0
ZDT@VAIO <dev@dthappy.com>
2017-10-26 12:38:31 +0800

    add top bar

--- name/good.js
+++ name/good.js
@@ -1,17 +1,26 @@
 (_=>{
   let btns = document.getElementsByClassName('addBtn');
   let keys= localStorage.getItem("words")?JSON.parse(localStorage.getItem("words")):[];
+  let selectedDiv;
   let saveWords = w=>{
     localStorage.setItem("words",JSON.stringify(keys));
   }
   let clearBtnClick = e=>{
-      console.log("CCC");
       keys = [];
       localStorage.setItem("words","[]");
       showWords();
+      if(keys.length==0) selectedDiv.style.margin = "0 0 -100px 0";
   }
   let addEventToClear = ()=>{
       document.getElementById("clear").addEventListener('touchend',clearBtnClick);
+  }
+  let addEventToAddBtn = ()=>{
+    Array.from(btns).forEach(i=>{
+      i.addEventListener('touchend', btnClick);
+    });
+    // for(let i=0; i<btns.length; i++){
+    //   btns[i].addEventListener('touchend', btnClick);
+    // }
   }
   let btnClick = (e)=>{
     //   console.log("HI");
@@ -25,19 +34,20 @@
     let wordDiv = document.createElement('div');
     wordDiv.id="selectedDiv";
     document.getElementsByTagName('body')[0].appendChild(wordDiv);
+    selectedDiv = document.getElementById("selectedDiv");   
     showWords();
+    if(keys.length==0) selectedDiv.style.margin = "0 0 -100px 0";
   };
   let showWords = w=>{
       let words = "";
       keys.forEach(w=>{
           words+=`<span class="selected">${w}</span>`; 
       });
-      document.getElementById("selectedDiv").innerHTML=`<div>已选字：<span id="clear">清空</span></div>${words}`;
+      selectedDiv.innerHTML =`<div><div>已选字：<span id="clear">清空</span></div>${words}</div>`;
+      selectedDiv.style.margin = "0";
       addEventToClear();
-  }
-  Array.from(btns).forEach((element) =>{
-    element.addEventListener('touchend', btnClick);
-  });
+  };
+  addEventToAddBtn();
   initWordDiv();
   console.log("AAAA");
 })();
